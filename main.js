b44fd40665b107011117b09e8c584847173ec0e2
mluna <mluna@cifpfbmoll.eu>
2021-01-25 20:44:08 +0100

    Template engine and layouts

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express()
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 connectDB()
-app.listen(3000)
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main', extname:'.hbs'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.use('/', require('./routes/index.'))
+
+app.listen(3000)
+
