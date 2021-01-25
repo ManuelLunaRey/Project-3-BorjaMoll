183623cbdc4a6bac23bad907c199d50353d0491e
mluna <mluna@cifpfbmoll.eu>
2021-01-25 22:14:51 +0100

    finish css

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+const path = require('path')
 const express = require('express');
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('expres-handlebars')
+dotenv.config({path: './config/config.env' })
+connectDB()
 const app = express()
+//login
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main', extname:'.hbs'}));
+app.set('view engine', 'handlebars');
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+// routes
+app.use('/', require('./routes/index'))
+ const PORT = process.env.PORT || 3000
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
