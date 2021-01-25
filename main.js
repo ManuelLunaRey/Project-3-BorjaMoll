e75f4f1d140139361dfb8ce99f346b592addf65c
mluna <mluna@cifpfbmoll.eu>
2021-01-25 20:24:51 +0100

    config finish

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express()
+const connectDB = require('./config/db')
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+connectDB()
 app.listen(3000)
 
 // Routes
