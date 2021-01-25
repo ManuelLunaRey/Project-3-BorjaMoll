9ed3671ef7c9cb0c99ffa5d0928281e91225ef23
mluna <mluna@cifpfbmoll.eu>
2021-01-25 22:29:30 +0100

    csss

--- config/db.js
+++ config/db.js
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try{
+        const conn =await mongoose.connect(process.env.MONGO_URI,{
+            userNewUrlParser: true,
+            useUnifiedTopology: true,
+            userFindAndModify: false
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+
+}catch (err) {
+    console.error(err)
+    process.exit(1)
+}
+}
+=======
 
 const mongoose = require('mongoose')
 
@@ -17,3 +36,4 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB
+>>>>>>> 9bec5c5c467beff627ee1fb540d9d19b64749a4b
--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+const path = require('path')
 const express = require('express');
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('expres-handlebars')
+dotenv.config({path: './config/config.env' })
+connectDB()
 const app = express()
+<<<<<<< HEAD
+//login
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main', extname:'.hbs'}));
+app.set('view engine', 'handlebars');
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+// routes
+app.use('/', require('./routes/index'))
+ const PORT = process.env.PORT || 3000
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+=======
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
@@ -16,3 +44,4 @@ app.use('/', require('./routes/index.'))
 
 app.listen(3000)
 
+>>>>>>> 9bec5c5c467beff627ee1fb540d9d19b64749a4b
