5cc3c67fa17cc122d01e3f433efc2bed438d5944
mluna <mluna@cifpfbmoll.eu>
2021-02-15 19:02:23 +0100

    Edit

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 const restaurant = require('../models/restaurant');
 // Posible query mostrar todo: const restaurants = await restaurant.find({ }).lean()
+// db.restaurants.find({"_id":ObjectId("6026b3662c792472219fbf6c")});
 //desc Login/Landin page
 //@route get /
 router.get('/', async (req, res) => {
     try{
         const restaurants = await restaurant.find().sort( { rate: -1 } ).limit(5).lean();
         res.render('home', {
+            restaurants,
+        })
+
+
+   } catch (err) {
+        console.error(err)
+        res.render('error/500')
+
+   }
+
+})
+//desc Login/Landin page
+//@route get /
+router.get('/admin', async (req, res) => {
+    try{
+        const restaurants = await restaurant.find().lean();
+        res.render('admin', {
             restaurants,
         })
 
@@ -31,4 +49,9 @@ router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
+// @desc agregar
+// @route GET /agregar
+router.get('/agregar', (req, res) => {
+    res.render('agregar')
+})
 module.exports = router;
